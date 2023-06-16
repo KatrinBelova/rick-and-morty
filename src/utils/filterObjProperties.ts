084ced9082fc2b6906984d868ffe09e93297ac41
KateBilova <kateryna.bilova@extrawest.com>
2023-06-16 17:46:06 +0300

    removed utils

--- src/utils/filterObjProperties.ts
+++
@@ -1,6 +0,0 @@
-export function filterProperties<T extends Record<string, unknown>>(
-  obj: T,
-  propertyNames: (keyof T)[]
-) {
-  return propertyNames.map((propertyName) => obj[propertyName]).filter(Boolean);
-}
